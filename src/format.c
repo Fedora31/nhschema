7ae72cdcb85f397c51452e12a0eceb95f63723bf
Fedora31 <61557224+Fedora31@users.noreply.github.com>
2023-03-26 16:50:16 +0200

    Add missing medic_backpack bodygroup

--- src/format.c
+++ src/format.c
@@ -7,14 +7,15 @@
 #include "parser.h"
 #include "format.h"
 
-#define B_HAT        0x1
-#define B_HEADPHONES 0x2
-#define B_DOGTAGS    0x4
-#define B_BACKPACK   0x8
-#define B_GRENADES   0x10
-#define B_HEAD       0x20
-#define B_SHOES      0x40
-#define B_HANDS      0x80
+#define B_HAT            0x1
+#define B_HEADPHONES     0x2
+#define B_DOGTAGS        0x4
+#define B_BACKPACK       0x8
+#define B_GRENADES       0x10
+#define B_HEAD           0x20
+#define B_SHOES          0x40
+#define B_HANDS          0x80
+#define B_MEDIC_BACKPACK 0x100
 #define ERRMASK 0x0
 
 static const Classinfo classinfo[] = {
@@ -25,7 +26,7 @@ static const Classinfo classinfo[] = {
 	{0x8,     "Demo",     "demo",     "Demoman",  DEMO,     B_SHOES | B_GRENADES}, /*see "Dr's Dapper Topper"*/
 	{0x10,    "Engineer", "engineer", "engineer", ENGINEER, B_HAT},
 	{0x20,    "Heavy",    "heavy",    "heavy",    HEAVY,    B_HANDS},
-	{0x40,    "Medic",    "medic",    "medic",    MEDIC,    B_BACKPACK},
+	{0x40,    "Medic",    "medic",    "medic",    MEDIC,    B_MEDIC_BACKPACK},
 	{0x80,    "Sniper",   "sniper",   "sniper",   SNIPER,   B_HAT},
 	{0x100,   "Spy",      "spy",      "spy",      SPY,      0},
 
@@ -33,16 +34,16 @@ static const Classinfo classinfo[] = {
 };
 
 static const Bodyinfo bodyinfo[] = {
-	{B_HAT,        "hat",        "hat"},
-	{B_HEADPHONES, "headphones", "headphones"},
-	{B_DOGTAGS,    "dogtags",    "dogtags"},
-	{B_BACKPACK,   "backpack",   "backpack"},
-	{B_BACKPACK,   "backpack",   "medic_backpack"},
-	{B_GRENADES,   "grenades",   "grenades"},
-	{B_HEAD,       "head",       "head"},
-	{B_SHOES,      "shoes",      "shoes"},
-	{B_SHOES,      "shoes",      "shoes_socks"},
-	{B_HANDS,      "hands",      "hands"},
+	{B_HAT,            "hat",        "hat"},
+	{B_HEADPHONES,     "headphones", "headphones"},
+	{B_DOGTAGS,        "dogtags",    "dogtags"},
+	{B_BACKPACK,       "backpack",   "backpack"},
+	{B_MEDIC_BACKPACK, "backpack",   "medic_backpack"},
+	{B_GRENADES,       "grenades",   "grenades"},
+	{B_HEAD,           "head",       "head"},
+	{B_SHOES,          "shoes",      "shoes"},
+	{B_SHOES,          "shoes",      "shoes_socks"},
+	{B_HANDS,          "hands",      "hands"},
 
 	{ERRMASK,      "Error",      "Error"}
 };
